9523fccb907765c62b23ffca080799884aa4d4f1
ganti143@gmail.com <ganti143@gmail.com>
2024-08-18 13:17:05 +0530

    Added Home Page

--- src/app/page-content/page-content.component.ts
+++ src/app/page-content/page-content.component.ts
@@ -21,6 +21,7 @@ ngOnInit():void {
   this.route.paramMap.subscribe((res?:any)=>{
     // console.log(res.params.path)
     const path = res.params.path;
+    console.log(path);
     this.srcPath = `md/${path}.md`
   })
 }
